7650cbea8393ecd1dc79ab570eef067367fccb8f
_bidhayak <rishikeyyadav@gmail.com>
2025-03-23 13:16:34 +0530

    DB credentials updated

--- src/main/java/com/example/translation_app/repository/wordRepository.java
+++ src/main/java/com/example/translation_app/repository/wordRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 @Repository
 public interface  wordRepository extends CrudRepository<Word,String>{
+
 }
--- src/main/java/com/example/translation_app/service/TranslationService.java
+++ src/main/java/com/example/translation_app/service/TranslationService.java
@@ -1,5 +1,6 @@
 package com.example.translation_app.service;
 
+import com.example.translation_app.RequestBodyDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -10,7 +11,11 @@ public class TranslationService {
     @Autowired
     RestClient restClient;  // field injection
 
-    public void getTranslation() {
+    public void getTranslation(RequestBodyDTO requestBodyDTO) {
+        //check in DB
+        //check in cache
+        //call Traslate API
+
 
     }
 }
